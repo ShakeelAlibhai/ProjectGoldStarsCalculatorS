99da4013aedca7bab0a754d77e83ffd733ea7ec0
ShakeelAlibhai <shakeel.alibhai419@gmail.com>
2020-01-20 15:19:26 -0500

    Add feet to distance converter

--- src/converters/DistanceConverter.java
+++ src/converters/DistanceConverter.java
@@ -1,9 +1,30 @@
 package converters;
 public class DistanceConverter
 {
+    public double toFeet(String fromType, double fromValue)
+    {
+        double f = 0.0;
+        
+        if(fromType.equals("Kilometers"))
+        {
+            fromValue = this.toMiles("Kilometers", fromValue);
+        }
+        
+        //At this point, assume that fromValue is in miles
+        
+        f = fromValue * 5280;
+        
+        return f;
+    }
+    
     public double toMiles(String fromType, double fromValue)
     {
         double m = 0.0;
+        
+        if(fromType.equals("Feet"))
+        {
+            m = fromValue / 5280;
+        }
         
         if(fromType.equals("Kilometers"))
         {
@@ -17,10 +38,14 @@ public class DistanceConverter
     {
         double k = 0.0;
         
-        if(fromType.equals("Miles"))
+        if(fromType.equals("Feet"))
         {
-            k = fromValue / 0.62137119;
+            fromValue = this.toMiles("Feet", fromValue);
         }
+        
+        //At this point, assume that fromValue is in miles
+        
+        k = fromValue / 0.62137119;
         
         return k;
     }
